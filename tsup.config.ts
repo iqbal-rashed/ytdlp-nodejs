1860032cce567bdefd85da18a2fcf676e8362f11
Rashed Iqbal <dev.rashediqbal@gmail.com>
2025-04-28 22:02:55 +0600

    add: init changes

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  splitting: false,
-  clean: true,
-  minify: true,
-  shims: true,
-});
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    splitting: false,
+    clean: true,
+    minify: true,
+    shims: true,
+  },
+  {
+    entry: ['src/scripts/download.ts'],
+    format: ['cjs'],
+    splitting: false,
+    clean: true,
+    minify: true,
+    shims: true,
+  },
+]);
